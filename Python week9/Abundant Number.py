6bebcef9ad66b5ea3a7169aeafc3cee50a1bdaf3
KARTHICK-231801079 <231801079@rajalakshmi.edu.in>
2024-06-13 20:06:39 +0530

    Create Abundant Number.py
    An abundant number is a number for which the sum of its proper divisors is greater than the number itself. Proper divisors of the number are those that are strictly lesser than the number.
    
    Input Format:
    Take input an integer from stdin
    Output Format:
    Return Yes if given number is Abundant. Otherwise, print No
    Example input:
    12
    Output:
    Yes
    Explanation
    The proper divisors of 12 are: 1, 2, 3, 4, 6, whose sum is 1 + 2 + 3 + 4 + 6 = 16. Since sum of proper divisors is greater than the given number, 12 is an abundant number.
    Example input:
    13
    Output:
    No
    Explanation
    The proper divisors of 13 is: 1, whose sum is 1. Since sum of proper divisors is not greater than the given number, 13 is not an abundant number.

---
+++ Python week9/Abundant Number.py
@@ -0,0 +1,7 @@
+Program:
+def abundant(number):
+    d_s=sum([divisor for divisor in range(1,number) if number % divisor == 0])
+    if d_s>number:
+        return"Yes"
+    else:
+        return "No"
