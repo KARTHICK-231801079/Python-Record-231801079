7304447e864cdc5036f041689f46b5f2a813e501
KARTHICK-231801079 <231801079@rajalakshmi.edu.in>
2024-06-13 20:18:59 +0530

    Create EXCEPTION HANDLING.py
    To find whether a digit lies in the specified range(1-100). Handling exceptions for invalid inputs and out-of-range numbers .
    Input Format:
    User inputs a number.
    Output Format:
    Confirm the input or print an error message if it's invalid or out of range.

---
+++ Python week11/EXCEPTION HANDLING.py
@@ -0,0 +1,9 @@
+#Program:
+try:
+    a=input()
+    if(int(a)>0 and int(a)<101):
+        print("Valid input.")
+    else:
+        print("Error: Number out of allowed range")
+except:
+    print("Error: invalid literal for int()")
