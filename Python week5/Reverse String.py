dc18d525cba005ff705795cbe451be1ceec58322
KARTHICK-231801079 <231801079@rajalakshmi.edu.in>
2024-06-13 19:29:30 +0530

    Create Reverse String.py
    Reverse a string without affecting special characters. Given a string S, containing special characters and all the alphabets, reverse the string without affecting the positions of the special characters.
    Input:
    A&B
    Output:
    B&A
    Explanation: As we ignore '&' and
    As we ignore '&' and then reverse, so answer is "B&A".
    For example:
    Input	Result
    A&x#
    x&A#

---
+++ Python week5/Reverse String.py
@@ -0,0 +1,16 @@
+#Program:
+s=input()
+l=[]
+for i in s:
+    if(i.isalpha()):
+        l.append(i)
+l.reverse()
+r=''
+index=0
+for i in s:
+    if(i.isalpha()):
+        r+=l[index]
+        index+=1
+    else:
+        r+=i
+print(r)
